f1dcb4633be75ad24c76761ea46fc6c29560c4d7
EBerk26 <eli_berk26@milton.edu>
2025-04-09 13:43:56 -0400

    myplayer now loads a file - but somehow that's slower than just finding the solutions.

--- src/MyPlayer.java
+++ src/MyPlayer.java
@@ -1,4 +1,11 @@
 import java.awt.*;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.Serializable;
+import java.lang.reflect.Array;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Hashtable;
@@ -22,129 +29,13 @@ public class MyPlayer {
     Hashtable<ArrayList<Integer>,orderedPair> solutions = new Hashtable<>();
 
     public MyPlayer() {
-            final long startTime = System.currentTimeMillis();
-            listGameStates(new ArrayList<Integer>(),boardLength);
-            totalGameStates = gameStates.size();
-            hashtable = new Hashtable<>();
-            hashtable.put(gameStates.get(0),true);
-            hashtable.put(gameStates.get(1),false);
-            listOfMoves = new orderedPair[totalGameStates];
-            listOfMoves[0] = new orderedPair(-1,-1);
-            listOfMoves[1] = new orderedPair(0,0);
-            for(int x=2; x<totalGameStates;x++){
-                if(listOfMoves[x] == null) {
-                    listOfMoves[x] = chooseBestMove(gameStates.get(x), x);
-                }
-            }
-            for(int x = 0; x<listOfMoves.length;x++){
-                solutions.put(gameStates.get(x),listOfMoves[x]);
-            }
+        try (ObjectInputStream ois = new ObjectInputStream(Files.newInputStream(Paths.get("/Users/eliberk/Desktop/CS2/Chomp/chompSolutions.ser")))) {
+            solutions = (Hashtable<ArrayList<Integer>, orderedPair>) ois.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
         }
-
-        void listGameStates(ArrayList<Integer> previousColumns, int max) {
-            if (previousColumns.size() == boardLength) {
-                gameStates.add(previousColumns);
-            } else {
-                for (int x = 0; x <= max; x++) {
-                    ArrayList<Integer> nextColumns = new ArrayList<>(previousColumns);
-                    nextColumns.add(x);
-                    listGameStates(nextColumns, x);
-                }
-            }
-        }
-
-
-        orderedPair chooseBestMove(ArrayList<Integer> gameState,int whichGameState) {
-            if(canMakeV(gameState)){
-                hashtable.put(gameState,true);
-                return new orderedPair(1,1);
-            }
-            totalPossibleMoves = 0;
-            for(int i: gameState){
-                totalPossibleMoves+=i;
-            }
-            possibleOrderedPairs = new orderedPair[totalPossibleMoves];
-            int i =0;
-            for (int x = 0; x < gameState.size(); x++) {
-                for (int y = 0; y< gameState.get(x); y++) {
-                    possibleOrderedPairs[i] = new orderedPair(x,y);
-                    i++;
-                }
-            }
-            for (orderedPair c : possibleOrderedPairs) {
-                gameStatePossibility = new ArrayList<>(gameState);
-                arrayVersion = gameStatePossibility.toArray();
-                for (int x = c.x; x < gameState.size(); x++) {
-                    if ((int) (arrayVersion[x]) >= c.y) {
-                        arrayVersion[x] = c.y;
-                    }
-                }
-                if(!hashtable.get(new ArrayList<>(Arrays.asList(arrayVersion)))){//if the choice has been previously marked as a loss state, play it and mark this one as a win.
-                    hashtable.put(gameState,true);
-                    //System.out.println("Solved: Game State "+(whichGameState+1)+"/"+totalGameStates+" total game states.");
-                    //go through and find any game state larger than this where the same move brings you to the same place
-               /*for(int x = whichGameState+1;x<gameStates.size();x++){
-                   if(listOfMoves[x]!=null){
-                       if(moveGivesSameResult(c,gameStates.get(x))){
-                           listOfMoves[x] = c;
-                       }
-                   }
-               }*/ //this slows you down because of the searching
-                    //mirrorImage(gameState,c,true); this also slows you down
-                    return c;
-                }
-            }
-            hashtable.put(gameState,false);
-            //play the move that is first, highest, and second, furthest to the right - this basically removes the fewest possible chips if you know you're going to lose
-            highestY = gameState.get(0);
-            for (int x = 0;x<gameState.size();x++){
-                if(gameState.get(x)<highestY){
-                    //System.out.println("Solved: Game State "+(whichGameState+1)+"/"+totalGameStates+" total game states.");
-                    leastRemoval = new orderedPair(x-1,highestY-1);
-                    mirrorImage(gameState,leastRemoval,false);
-                    return leastRemoval;
-                }
-            }
-            //System.out.println("Solved: Game State "+(whichGameState+1)+"/"+totalGameStates+" total game states.");
-            return new orderedPair(0,0);
-        }
-        void mirrorImage(ArrayList<Integer> gameState,orderedPair solution, boolean isWin){ //solve the mirror image case
-            ArrayList<Integer> mirroredGameState = new ArrayList<>();
-            for(int y =0;y<gameState.get(0);y++){
-                int i = 0;
-                int x = 0;
-                while(x<gameState.size()&&gameState.get(x)>y){
-                    i++;
-                    x++;
-                }
-                mirroredGameState.add(i);
-            }
-            while(mirroredGameState.size()<gameState.size()){
-                mirroredGameState.add(0);
-            }
-            index = gameStates.indexOf(mirroredGameState);
-            listOfMoves[index] = solution.flip();
-            hashtable.put(mirroredGameState,isWin);
-        }
-        @SuppressWarnings("unused")
-        boolean canMakeV(ArrayList<Integer> gameState){
-            if(gameState.get(1)<2){
-                return false;
-            }
-            numberInFirstColumn = gameState.get(0);
-            for (int x =0;x<gameState.size();x++){
-                if(x<=numberInFirstColumn-1){
-                    if(gameState.get(x)<1){
-                        return false;
-                    }
-                } else {
-                    if (gameState.get(x)!=0){
-                        return false;
-                    }
-                }
-            }
-            return true;
-        }
+        System.out.println();
+    }
         /***
          * This code will run just once, when the game opens.
          * Add your code here.
@@ -186,7 +77,7 @@ public class MyPlayer {
          */
     }
 }
-class orderedPair {
+class orderedPair implements Serializable{
     int x;
     int y;
 
